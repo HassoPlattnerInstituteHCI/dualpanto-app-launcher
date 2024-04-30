5ddab601f6487b1a346a60c3dff6fa0c806094d5
Nick <nick@henjes-net.de>
2024-04-30 15:21:17 +0200

    changed folder structure

---
+++ Assets/unity-dualpanto-framework/Assets/ExampleScripts/ObstacleManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using DualPantoFramework;
+public class ObstacleManager : MonoBehaviour
+{
+    PantoCollider[] pantoColliders;
+    void Start()
+    {
+        Invoke("createObstacle", 1.0f);
+    }
+
+    private void createObstacle()
+    {
+        pantoColliders = GameObject.FindObjectsOfType<PantoCollider>();
+        foreach (PantoCollider collider in pantoColliders)
+        {
+            collider.CreateObstacle();
+            collider.Enable();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (PantoCollider collider in pantoColliders)
+            {
+                collider.Enable();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach (PantoCollider collider in pantoColliders)
+            {
+                collider.Disable();
+            }
+        }
+    }
+}
