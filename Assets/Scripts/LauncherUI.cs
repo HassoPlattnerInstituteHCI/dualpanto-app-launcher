49399447ff15769669b50e926d55ab4cd64a1a8d
Nick <nick@henjes-net.de>
2024-05-18 01:55:35 +0200

    added support for mac

--- Assets/Scripts/LauncherUI.cs
+++ Assets/Scripts/LauncherUI.cs
@@ -22,20 +22,42 @@ public class LauncherUI : MonoBehaviour
     // }
 
     public void startForceField() {
+
+        #if UNITY_STANDALONE_WIN
         // change ".exe" in data path to ".app" for mac
-        string pathForceField = Application.dataPath + "/../Builds/ForceField/ForceField.exe";
+            string pathForceFieldW = Application.dataPath + "/../Builds/ForceField/ForceField.exe";
         // Process.Start should also work on mac (hopefully)
-        Process.Start(pathForceField);
+            Process.Start(pathForceFieldW);
+        #endif
+
+        #if UNITY_STANDALONE_OSX
+            string pathForceFieldO = Application.dataPath + "/Games/ForceField.app";
+            Process.Start(pathForceFieldO);
+        #endif
     }
 
     public void startMovement() {
-        string pathMovement = Application.dataPath + "/../Builds/Movement/Movement.exe";
-        Process.Start(pathMovement);
+        #if UNITY_STANDALONE_WIN
+            string pathMovementW = Application.dataPath + "/../Builds/Movement/Movement.exe";
+            Process.Start(pathMovementW);
+        #endif
+
+        #if UNITY_STANDALONE_OSX
+            string pathMovementO = Application.dataPath + "/Games/Movement.app";
+            Process.Start(pathMovementO);
+        #endif
     }
 
     public void startObstacles() {
-        string pathObstacles = Application.dataPath + "/../Builds/Obstacles/Obstacles.exe";
-        Process.Start(pathObstacles);
+        #if UNITY_STANDALONE_WIN
+            string pathObstaclesW = Application.dataPath + "/../Builds/Obstacles/Obstacles.exe";
+            Process.Start(pathObstaclesW);
+        #endif
+
+        #if UNITY_STANDALONE_OSX
+            string pathObstaclesO = Application.dataPath + "/Games/Obstacles.app";
+            Process.Start(pathObstaclesO);
+        #endif
     }
 
 }
