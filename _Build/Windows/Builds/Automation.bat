600cc8356a8dc5e5beed1f515451acd3cde4bd14
Nick <nick@henjes-net.de>
2024-07-07 23:59:44 +0200

    moved file

--- _Build/Windows/Builds/Automation.bat
+++ _Build/Windows/Builds/Automation.bat
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 
 cd Builds
-set items= 1 2 3 4 5
-@REM  6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+set items= 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
 
 for %%i in (%items%) do (
+    @REM Only works for teams <10 rn
     set "team=team-0%%i"
     set "folder=%%i"
     @REM echo !team!
